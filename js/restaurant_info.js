0f7d74752e3799e4d3aee106f4e59b2c3e47a6f4
howardop <hlo4@cornell.edu>
2018-10-03 00:17:36 -0400

    Responsive index.html

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiaG93YXJkbyIsImEiOiJjam1zNzh5N3MwMDluM3Buc3V6NTNkYTl1In0.fEBus8AVVm1o6Kj7Dt5QKQ',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
